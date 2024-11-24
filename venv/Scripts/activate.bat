a15fced63544ec2651da4cb5f2d92629ea5e4f84
Petrucio Alberto <petruciolima2406@hotmail.com>
2024-11-23 21:07:46 -0300

    Inicializando o repositório para deploy no Heroku

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,15 +8,15 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Alexi\OneDrive\Documentos\FINANCE_PSW\venv
+set VIRTUAL_ENV=C:\Users\Auditeste0164\Desktop\projeto\Pratica-Mackenzie-5-Semestre\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
 if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
-set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(venv) %PROMPT%
+set "_OLD_VIRTUAL_PROMPT=%PROMPT%"
+set "PROMPT=(venv) %PROMPT%"
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,7 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(venv) 
+set VIRTUAL_ENV_PROMPT=venv
 
 :END
 if defined _OLD_CODEPAGE (
